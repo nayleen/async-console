1f61741f37356865171323a7d58ebe90c416aac2
Nayleen <almightynay@gmail.com>
2023-09-25 00:05:28 +0200

    Integrate with async Component TestCase

--- tests/Console/ComponentTest.php
+++ tests/Console/ComponentTest.php
@@ -4,11 +4,30 @@ declare(strict_types = 1);
 
 namespace Nayleen\Async\Console;
 
-use Amp\PHPUnit\AsyncTestCase;
+use Nayleen\Async\Component\TestCase;
+use Nayleen\Async\Console\Command\Loader as CommandLoader;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\CommandLoader\CommandLoaderInterface;
 
 /**
  * @internal
  */
-final class ComponentTest extends AsyncTestCase
+final class ComponentTest extends TestCase
 {
+    protected function component(): Component
+    {
+        return new Component();
+    }
+
+    /**
+     * @test
+     */
+    public function registers_console(): void
+    {
+        $this->assertContainerHasService(Application::class);
+        $this->assertContainerHasService(CommandLoaderInterface::class, CommandLoader::class);
+
+        $console = $this->container()->get(Application::class);
+        self::assertSame('Kernel Console', $console->getName());
+    }
 }
